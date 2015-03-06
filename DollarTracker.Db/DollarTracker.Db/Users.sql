436714dbbec41efd2c0fa9b1adde51bb439403e3
alphaCoder <srao2013@gmail.com>
2015-03-05 18:34:26 -0600

    db setup

---
+++ DollarTracker.Db/DollarTracker.Db/Users.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Users]
+(
+	[UserId] UNIQUEIDENTIFIER NOT NULL PRIMARY KEY, 
+    [Username] VARCHAR(50) NOT NULL, 
+    [Password] VARCHAR(40) NOT NULL, 
+    [PasswordSalt] UNIQUEIDENTIFIER NOT NULL, 
+    [CreatedDtUtc] DATETIME2 NOT NULL, 
+    [Status] BIT NOT NULL
+)
