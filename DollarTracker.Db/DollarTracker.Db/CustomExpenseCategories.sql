8aaa2141d999dac444ba0fbf5740efc366acbd70
alphaCoder <srao2013@gmail.com>
2015-03-05 21:44:54 -0600

    adding foriegn key contraints and adding new CustomExpenseCategories table

---
+++ DollarTracker.Db/DollarTracker.Db/CustomExpenseCategories.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[CustomExpenseCategories]
+(
+	[CustomExpenseCategoryId] VARCHAR(20) NOT NULL PRIMARY KEY, 
+    [UserId] UNIQUEIDENTIFIER NOT NULL, 
+    [Description] VARCHAR(50) NOT NULL, 
+    [CreatedDt] DATETIME2 NULL, 
+    [Status] BIT NOT NULL DEFAULT 0, 
+    CONSTRAINT [FK_CustomExpenseCategories_ToUsers] FOREIGN KEY ([UserId]) REFERENCES [Users]([UserId]) 
+)
--- DollarTracker.Db/DollarTracker.Db/ExpenseStories.sql
+++ DollarTracker.Db/DollarTracker.Db/ExpenseStories.sql
@@ -7,5 +7,6 @@
 	[CreatedBy] UNIQUEIDENTIFIER NOT NULL, 
     [StartDt] DATETIME2 NOT NULL, 
     [EndDt] DATETIME2 NOT NULL, 
-    [CreatedUtcDt] DATETIME2 NOT NULL
+    [CreatedUtcDt] DATETIME2 NOT NULL, 
+    CONSTRAINT [FK_ExpenseStories_ToExpenseStoryTypes] FOREIGN KEY ([ExpenseStoryTypeId]) REFERENCES [ExpenseStoryTypes]([ExpenseStoryTypeId])
 )
