2e43be8e91356f729f8fb86e1bf7ff6088af2a6e
alphaCoder <srao2013@gmail.com>
2015-03-07 14:29:54 -0600

    add UserManager, UserRepository

---
+++ DollarTracker.Net/DollarTracker.Core/Managers/UserManager.cs
@@ -0,0 +1,61 @@
+﻿using DollarTracker.Core.Infrastructure;
+using DollarTracker.Core.Repository;
+using DollarTracker.EF;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DollarTracker.Core.Managers
+{
+	public interface IUserManager
+	{
+		void AddUser(Users user);
+		void UpdateUser(Users user);
+		Users GetUser(Guid userId);
+		void SaveUser();
+	}
+	public class UserManager : IUserManager
+	{
+		private readonly IUserRepository userRepository;
+		private readonly IUnitOfWork unitOfWork;
+		public UserManager(IUserRepository userRepository, IUnitOfWork unitOfWork)
+		{
+			this.userRepository = userRepository;
+			this.unitOfWork = unitOfWork;
+		}
+		public void AddUser(Users user)
+		{
+			var existingUser = userRepository.Get(x => x.Email == user.Email);
+			if (existingUser == null)
+			{
+				userRepository.Add(user);
+				SaveUser();
+			}
+		}
+
+		public void UpdateUser(Users user)
+		{
+			var existingUser = userRepository.Get(x => x.Email == user.Email);
+			if (existingUser != null)
+			{
+				existingUser.Password = user.Password;
+				existingUser.Status = user.Status;
+				userRepository.Add(existingUser);
+				SaveUser();
+			};
+		}
+
+		public Users GetUser(Guid userId)
+		{
+			return userRepository.Get(x => x.UserId == userId);
+		}
+
+
+		public void SaveUser()
+		{
+			unitOfWork.Save();
+		}
+	}
+}
---
+++ DollarTracker.Net/DollarTracker.Core/Repository/UserRepository.cs
@@ -0,0 +1,23 @@
+﻿using DollarTracker.EF;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DollarTracker.Core.Infrastructure;
+
+namespace DollarTracker.Core.Repository
+{
+	public interface IUserRepository: IRepository<Users>
+	{
+
+	}
+	public class UserRepository: RepositoryBase<Users>, IUserRepository
+	{
+		public UserRepository(IDbFactory dbFactory)
+			: base(dbFactory)
+		{
+
+		}
+	}
+}
