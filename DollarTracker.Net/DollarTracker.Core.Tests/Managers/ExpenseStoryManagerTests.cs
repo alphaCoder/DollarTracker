5a6991c954e52cb8384d0d9d9525ae89dc1fc816
alphaCoder <srao2013@gmail.com>
2015-03-08 23:13:47 -0500

    delete edmx build break

---
+++ DollarTracker.Net/DollarTracker.Core.Tests/Managers/ExpenseStoryManagerTests.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DollarTracker.EF;
+using DollarTracker.Core.Managers;
+using DollarTracker.Core.Repository;
+using DollarTracker.EF;
+
+namespace DollarTracker.Core.Tests.Managers
+{
+	/// <summary>
+	/// Summary description for ExpenseManagerTests
+	/// </summary>
+	[TestClass]
+	public class ExpenseStoryManagerTests : TestEnvironmentBase
+	{
+		private User user;
+		private ExpenseStoryRepository expenseStoryRepository;
+		private ExpenseStoryManager expenseStoryManager;
+		public ExpenseStoryManagerTests()
+		{
+			user = GetNewMockUser();
+			new UserManager(new UserRepository(dbFactory), unitOfWork).AddUser(user);
+			expenseStoryRepository = new ExpenseStoryRepository(dbFactory);
+			expenseStoryManager = new ExpenseStoryManager(expenseStoryRepository, unitOfWork);
+		}
+
+
+		[TestMethod]
+		public void TestMethod1()
+		{
+			//
+			// TODO: Add test logic here
+			//
+		}
+
+		private void GetNewMockExpenseStory()
+		{
+			var expenseStory = new ExpenseStory
+			{
+
+			};
+		}
+
+	}
+}
---
+++ DollarTracker.Net/DollarTracker.Core/Repository/ExpenseStoryTypeRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DollarTracker.Core.Infrastructure;
+using DollarTracker.EF;
+namespace DollarTracker.Core.Repository
+{
+	public interface IExpenseStoryRepository : IRepository<ExpenseStoryType> { }
+
+	public class ExpenseStoryTypeRepository : RepositoryBase<ExpenseStoryType>, IExpenseStoryRepository
+	{
+		public ExpenseStoryTypeRepository(IDbFactory dbFactory)
+			: base(dbFactory)
+		{
+
+		}
+	}
+}
