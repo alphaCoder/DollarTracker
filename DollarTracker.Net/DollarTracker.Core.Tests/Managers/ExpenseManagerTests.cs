026ceaadfeefc854d99e364aae104d9cb05232a8
alphaCoder <srao2013@gmail.com>
2015-03-10 18:48:03 -0500

    expense manager tests

--- DollarTracker.Net/DollarTracker.Core.Tests/Managers/ExpenseManagerTests.cs
+++ DollarTracker.Net/DollarTracker.Core.Tests/Managers/ExpenseManagerTests.cs
@@ -7,6 +7,7 @@ using DollarTracker.Core.Infrastructure;
 using DollarTracker.Core.Repository;
 using DollarTracker.Core.Managers;
 using DollarTracker.Core.Utils;
+using System.Linq;
 
 namespace DollarTracker.Core.Tests.Managers
 {
@@ -32,16 +33,71 @@ namespace DollarTracker.Core.Tests.Managers
 			//todo: replace with collaborator manager
 			dataContext.Collaborator.Add(collaborator);
 			dataContext.SaveChanges();
+			expenseRepository = new ExpenseRepository(dbFactory);
+			expenseManager = new ExpenseManager(expenseRepository, unitOfWork);
 		}
 
 		[TestMethod]
 		public void AddExpenseTest()
 		{
-			//
-			// TODO: Add test logic here
-			//
+			var expectedExpense = GetNewMockExpense(collaborator.CollaboratorId, expenseStory.ExpenseStoryId);
+
+			expenseManager.AddExpense(expectedExpense);
+			var acutalExpense = dataContext.Expense.FirstOrDefault(x => x.ExpenseId == expectedExpense.ExpenseId);
+
+			Assert.IsNotNull(acutalExpense);
+			Assert.AreEqual(expectedExpense.GetHashCode(), acutalExpense.GetHashCode());
 		}
 
+		[TestMethod]
+		public void UpdateExpenseTest()
+		{
+			var expense = GetNewMockExpense(collaborator.CollaboratorId, expenseStory.ExpenseStoryId);
+
+			expenseManager.AddExpense(expense);
+			expense.Amount = 500000;
+			expenseManager.UpdateExpense(expense);
+
+			var updatedExpense = dataContext.Expense.FirstOrDefault(x => x.ExpenseId == expense.ExpenseId);
+
+			Assert.IsNotNull(updatedExpense);
+			Assert.AreEqual(expense.Amount, updatedExpense.Amount);
+		}
+
+		[TestMethod]
+		public void GetAllExpensesTest()
+		{
+			var expense1 = GetNewMockExpense(collaborator.CollaboratorId, expenseStory.ExpenseStoryId);
+			var expense2 = GetNewMockExpense(collaborator.CollaboratorId, expenseStory.ExpenseStoryId);
+			var expense3 = GetNewMockExpense(collaborator.CollaboratorId, expenseStory.ExpenseStoryId);
+
+			var expectedExpenseCount = 3;
+			expenseManager.AddExpense(expense1);
+			expenseManager.AddExpense(expense2);
+			expenseManager.AddExpense(expense3);
+
+			var allExpenses = expenseManager.GetAllExpenses(expenseStory.ExpenseStoryId);
+
+			Assert.IsNotNull(allExpenses);
+			Assert.AreEqual(expectedExpenseCount, allExpenses.Count());
+ 
+		}
+
+		[TestMethod]
+		public void DeleteExpenseTest()
+		{
+			var expense1 = GetNewMockExpense(collaborator.CollaboratorId, expenseStory.ExpenseStoryId);
+			var expense2 = GetNewMockExpense(collaborator.CollaboratorId, expenseStory.ExpenseStoryId);
+			var expense3 = GetNewMockExpense(collaborator.CollaboratorId, expenseStory.ExpenseStoryId);
+
+			expenseManager.AddExpense(expense1);
+			expenseManager.AddExpense(expense2);
+			expenseManager.AddExpense(expense3);
+
+			expenseManager.DeleteExpense(expense1.ExpenseId);
+			var isExists = dataContext.Expense.Any(x => x.ExpenseId == expense1.ExpenseId);
+			Assert.IsFalse(isExists);
+		}
 
 		private Expense GetNewMockExpense(Guid collaboratorId, string expenseStoryId)
 		{
@@ -51,7 +107,8 @@ namespace DollarTracker.Core.Tests.Managers
 				CreatedUtcDt = DateTime.UtcNow,
 				ExpenseStoryId = expenseStoryId,
 				CollaboratorId = collaboratorId,
-				ExpenseCategoryId = "Groceries"
+				ExpenseCategoryId = "Groceries",
+				Amount = 30000
 			};
 		}
 
--- DollarTracker.Net/DollarTracker.Core/Utils/UniqueKeyGenerator.cs
+++ DollarTracker.Net/DollarTracker.Core/Utils/UniqueKeyGenerator.cs
@@ -8,22 +8,18 @@ namespace DollarTracker.Core.Utils
 {
 	public class UniqueKeyGenerator
 	{
-		private static readonly int longKeyLen = 40;
-		private static readonly int shortKeyLen = 40;
+		private static readonly int shortKeyLen = 20;
 		public static string DatePrefixShortKey()
 		{
 			//todo: need to double check about possible key collision
 			return DatePrefixKey(shortKeyLen);
 		}
-		public static string DatePrefixLongKey()
-		{
-			return DatePrefixKey(longKeyLen);
-		}
 		public static string DatePrefixKey(int len = 20)
 		{
 			string months = "123456789ABC";
 			var n = DateTime.Now;
-			return (n.Year.ToString() + months[n.Month - 1].ToString() + Guid.NewGuid().ToString("N")).Substring(0, len);
+			var m = months[n.Month - 1].ToString();
+			return (n.Year.ToString() + m + Guid.NewGuid().ToString("N")).Substring(0, len);
 		}
 	}
 }
