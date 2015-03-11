e630fd36616e9355ca9a02822a34bcfc30525b84
alphaCoder <srao2013@gmail.com>
2015-03-10 19:54:02 -0500

    adding foreign key to collaborator table

--- DollarTracker.Db/DollarTracker.Db/Collaborator.sql
+++ DollarTracker.Db/DollarTracker.Db/Collaborator.sql
@@ -2,7 +2,8 @@
 (
 	[CollaboratorId] UNIQUEIDENTIFIER NOT NULL PRIMARY KEY, 
     [UserId] UNIQUEIDENTIFIER NOT NULL, 
-    [UserStoryId] VARCHAR(20) NOT NULL, 
+    [ExpenseStoryId] VARCHAR(20) NOT NULL, 
     [CreatedUtcDt] DATETIME2 NOT NULL, 
-    [Status] BIT NOT NULL
+    [Status] BIT NOT NULL, 
+    CONSTRAINT [FK_Collaborator_ToExpenseStory] FOREIGN KEY ([ExpenseStoryId]) REFERENCES [ExpenseStory]([ExpenseStoryId])
 )
