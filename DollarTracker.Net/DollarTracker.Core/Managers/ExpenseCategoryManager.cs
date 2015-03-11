efd3dbf7a8c75bd6073f8f9a6514d29936b8a461
alphaCoder <srao2013@gmail.com>
2015-03-10 23:05:35 -0500

    ninject di resolver

---
+++ DollarTracker.Net/DollarTracker.Core/Managers/ExpenseCategoryManager.cs
@@ -0,0 +1,32 @@
+﻿using DollarTracker.Core.Infrastructure;
+using DollarTracker.Core.Repository;
+using DollarTracker.EF;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DollarTracker.Core.Managers
+{
+	public interface IExpenseCategoryManager
+	{
+		IEnumerable<ExpenseCategory> GetAllExpenseCategories();  
+		//todo: other methods
+	}
+	public class ExpenseCategoryManager : IExpenseCategoryManager
+	{
+		private IUnitOfWork unitOfWork;
+		private IExpenseCategoryRepository expenseCategoryRepository;
+
+		public ExpenseCategoryManager(IExpenseCategoryRepository expenseCategoryRepository, IUnitOfWork unitOfWork)
+		{
+			this.expenseCategoryRepository = expenseCategoryRepository;
+			this.unitOfWork = unitOfWork;
+		}
+		public IEnumerable<ExpenseCategory> GetAllExpenseCategories()
+		{
+			return expenseCategoryRepository.GetAll();
+		}
+	}
+}
---
+++ DollarTracker.Net/DollarTracker.Web/ApiControllers/ExpenseCategoryController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+using DollarTracker.Core.Managers;
+using DollarTracker.EF;
+
+namespace DollarTracker.Web.ApiControllers
+{
+    public class ExpenseCategoryController : ApiController
+    {
+		private IExpenseCategoryManager expenseCategoryManager;
+		public ExpenseCategoryController(IExpenseCategoryManager expenseCategoryManager)
+		{
+			this.expenseCategoryManager = expenseCategoryManager;
+		}
+		public IEnumerable<ExpenseCategory> Get()
+		{
+			var categories = expenseCategoryManager.GetAllExpenseCategories();
+			return categories;
+		}
+    }
+}
