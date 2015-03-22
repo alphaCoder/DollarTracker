62d189c58d2d38d0703ce7a641afe3a17d98e07f
alphaCoder <srao2013@gmail.com>
2015-03-22 01:10:18 -0500

    adding angularjs functionality and updating few js packages

---
+++ DollarTracker.Net/DollarTracker.Web/app/app.js
@@ -0,0 +1,11 @@
+﻿app = angular.module('DollarTrackerApp', ['ui.router', 'ui.bootstrap']);
+
+app.config(['$stateProvider', '$urlRouterProvider', '$locationProvider', '$httpProvider', function ($stateProvider, $urlRouterProvider, $locationProvider, $httpProvider) {
+
+    $locationProvider.html5Mode(false);
+    $urlRouterProvider.otherwise("/");
+    //$httpProvider.interceptors.push('auth');
+
+}]);
+
+app.run([]); //todo: comeback
