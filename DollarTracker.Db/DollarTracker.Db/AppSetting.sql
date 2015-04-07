66312730ad2dce3af6d57aca4ca3dae88c7ee588
alphaCoder <srao2013@gmail.com>
2015-04-06 23:16:01 -0500

    changing app setting

--- DollarTracker.Db/DollarTracker.Db/AppSetting.sql
+++ DollarTracker.Db/DollarTracker.Db/AppSetting.sql
@@ -2,5 +2,6 @@
 (
 	[AppSettingId] UNIQUEIDENTIFIER NOT NULL PRIMARY KEY, 
     [Name] VARCHAR(50) NOT NULL, 
-    [Description] VARCHAR(100) NULL
+    [Description] VARCHAR(255) NULL, 
+    [Value] VARCHAR(255) NOT NULL
 )
