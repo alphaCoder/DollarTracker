acc062f944ecb69cb0fc8c5a9477f60cdf248b1f
alphaCoder <srao2013@gmail.com>
2015-04-07 23:47:20 -0500

    login, register ui, adding jasmine tests

--- DollarTracker.Net/DollarTracker.Web/app/app.js
+++ DollarTracker.Net/DollarTracker.Web/app/app.js
@@ -7,21 +7,29 @@ app.config(['$stateProvider', '$urlRouterProvider', '$locationProvider', '$httpP
    $urlRouterProvider.otherwise("/");
     //$httpProvider.interceptors.push('auth');
 
-    $stateProvider
-    .state('/', {
-        url: '/',
-        templateUrl: 'app/dashboard/dashboard.html?random=' + RANDOM,
-        controller: 'dashboardCtrl',
-        resolve: {
-            expenseStories: ['dashboard',  function (dashboard) {
-                return dashboard.getExpenseStories().then(function (results) {
-                    console.log("resolve");
-                    console.log(results);
-                    return results.data.data;
-                });
-            }]
-        }
-    });
+   $stateProvider
+   .state('/', {
+       url: '/',
+       templateUrl: 'app/dashboard/dashboard.html?random=' + RANDOM,
+       controller: 'dashboardCtrl',
+       resolve: {
+           expenseStories: ['dashboard', function (dashboard) {
+               return dashboard.getExpenseStories().then(function (results) {
+                   console.log("resolve");
+                   console.log(results);
+                   return results.data.data;
+               });
+           }]
+       }
+   })
+   .state('login', {
+       url: '/login',
+       templateUrl: 'app/user/login.html?random=' + RANDOM
+   })
+   .state('register', {
+       url: '/register',
+       templateUrl: 'app/user/register.html?random=' + RANDOM
+   });
 
 }]);
 
