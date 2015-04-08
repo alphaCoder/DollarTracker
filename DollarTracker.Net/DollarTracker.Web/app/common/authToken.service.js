5f74a187341b5bd55833541610b3e2972aa6ffa6
alphaCoder <srao2013@gmail.com>
2015-04-08 00:24:43 -0500

    auth token service

---
+++ DollarTracker.Net/DollarTracker.Web/app/common/authToken.service.js
@@ -0,0 +1,23 @@
+﻿'use strict';
+
+app.factory('authTokenService', function ($window) {
+
+    var storage = $window.localStorage;
+    var cachedToken;
+    return {
+        setToken: function (token) {
+            cachedToken = token;
+            storage.setItem('userToken', token);
+        },
+        getToken: function () {
+            if (!cachedToken) {
+                cachedToken = storage.getItem('userToken');
+
+                return cachedToken;
+            }
+        },
+        isAuthenticated: function () {
+            return !!this.getToken();
+        }
+    }
+})
